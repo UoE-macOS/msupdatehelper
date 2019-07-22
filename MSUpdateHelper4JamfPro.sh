9def98f5e5b2f24f98d310fdabdb98bc95c78494
Geoff Lee <g.lee@ed.ac.uk>
2019-07-22 12:04:33 +0100

    Running as root doesn't work in MAU 4.13, possibly earlier

--- MSUpdateHelper4JamfPro.sh
+++ MSUpdateHelper4JamfPro.sh
@@ -190,7 +190,8 @@ function DetermineLoginState() {
 	CONSOLE=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
 	if [ "$CONSOLE" == "" ]; then
     	echo "No user logged in"
-		CMD_PREFIX=""
+		# Running as root is broken: see comments in https://docs.microsoft.com/en-us/deployoffice/mac/update-office-for-mac-using-msupdate
+		CMD_PREFIX="sudo -u nobody"
 	else
     	echo "User $CONSOLE is logged in"
     	CMD_PREFIX="sudo -u $CONSOLE "
